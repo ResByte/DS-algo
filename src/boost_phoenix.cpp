2dddb5043d0ea09111207fbe6a36b8dce396e440
Abhinav Dadhich <abhi@iitj.ac.in>
2016-03-13 18:01:08 +0900

    add phoenix example

---
+++ src/boost_phoenix.cpp
@@ -0,0 +1,24 @@
+/*
+This is code for boost::phoenix tutorial. 
+It is part of functional programming paradigm in boost. 
+Using Boost.Phoenix it is possible to return another function. 
+*/
+#include <boost/phoenix/phoenix.hpp>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+bool is_odd(int i ) { return i % 2 ==1;}
+
+int main()
+{
+	std::vector<int> v{1,2,3,4,5};
+	std::cout << std::count_if(v.begin(),v.end(), is_odd) << '\n';
+	auto lambda = [](int i){return i%2 ==1;};
+	std::cout << std::count_if(v.begin(), v.end(),lambda)<< '\n';
+
+	using namespace boost::phoenix::placeholders;
+	auto phoenix = arg1 % 2== 1;
+	std::cout << std::count_if(v.begin(),v.end(),phoenix) << '\n';
+	return 0;
+}
